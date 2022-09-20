8f804d0d10c34ff4471fd4b5c259729f98cd10ea
champ09D <darshanmhatre709@gmail.com>
2022-09-20 21:12:31 +0530

    Added : Create Servlet File Case Add

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,49 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(
+        description = "Login Servlet Testing",
+        urlPatterns = { "/LoginServlet" },
+        initParams = {
+                @WebInitParam(name = "user", value = "Darsh"),
+                @WebInitParam(name = "password", value = "Mhatre@")
+        }
+)
+
+public class LoginServlet extends HttpServlet{
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String user = req.getParameter("user");
+        String pwd = req.getParameter("pwd");
+
+
+        String userID = getServletConfig().getInitParameter("user");
+        String password = getServletConfig().getInitParameter("password");
+
+        String regexName = "^[A-Z]{1}[a-zA-Z]{2,}$";
+        String regexPassword = "^(?=.*[0-9])(?=.*[@#$%^&+=])(?=.*[a-z])(?=.*[A-Z]){8,}$";
+
+
+        if(userID.equals(user) && userID.matches(regexName) && password.equals(pwd) && password.equals(pwd)) {
+            req.setAttribute("user",user);
+            req.getRequestDispatcher("LoginSuccess.jsp").forward(req, resp);
+        } else {
+            RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+            PrintWriter out  = resp.getWriter();
+            out.println("<font color = red> Either username or password is wrong</font>");
+            rd.include(req, resp);
+        }
+
+    }
+
+}
